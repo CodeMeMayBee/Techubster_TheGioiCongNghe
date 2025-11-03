46335a25240416cd4df4d1f4496af20cbe2e5802
CodeMeMayBee <codememaybe04@gmail.com>
2025-11-03 12:11:38 +0700

    Fix categories

--- assets/js/trangchu.js
+++ assets/js/trangchu.js
@@ -1,8 +1,8 @@
 window.addEventListener("DOMContentLoaded", function() {
-    addImgBanner("/thegioicongnghe-frontend/assets/img/banners/banner0.gif", "/thegioicongnghe-frontend/assets/img/banners/banner0.gif");
+    addImgBanner("/Techubster_TheGioiCongNghe/assets/img/banners/banner0.gif", "/Techubster_TheGioiCongNghe/assets/img/banners/banner0.gif");
     var numBanners = 9;
     for(var i = 1; i <= numBanners; i++) {
-        var imglink = "/thegioicongnghe-frontend/assets/img/banners/banner" + i + ".png";
+        var imglink = "/Techubster_TheGioiCongNghe/assets/img/banners/banner" + i + ".png";
         addImgBanner(imglink, imglink);
     }
     bannerSwiper(".banner__large-swiper", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev"); //Animation Banner
